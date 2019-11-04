9b40691eee37191d60c43efffc08098861efe7e7
Vinoshen Silva <VinoshenSilva@gmail.com>
2019-11-04 23:00:29 +0800

    Added create task and update task function

--- api/controller/task.controller.js
+++ api/controller/task.controller.js
@@ -2,5 +2,5 @@ const Task = require('../model/task.model');
 const mongoose = require('mongoose');
 
 exports.createTask = function(req,res){
-    
+
 };
--- api/model/task.model.js
+++ api/model/task.model.js
@@ -2,7 +2,58 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let taskSchema = new Schema({
-  _id: mongoose.Schema.Types.ObjectId
+  _id: mongoose.Schema.Types.ObjectId,
+  status: String,
+  description: String,
+  name: {type: String, enum: ['In Progress', 'Done', 'Completed']},
+  dependentTask: [taskSchema],
+  parentTask: mongoose.Schema.Types.ObjectId
 });
+
+taskSchema.statics.createTask = function(task){
+    return new Promise((resolve,reject)=>{
+    
+        let doc = new this({
+            _id: task._id,
+            name: task.name,
+            status: task.status,
+            description: task.description
+        });
+        
+        doc.save()
+        .then((result)=>{
+            resolve(result);
+        })
+        .catch((err)=>{
+            reject(err);
+        });
+    });
+}
+
+taskSchema.statics.updateTask = function(task){
+    return new Promise((resolve,reject)=>{
+        this.model("Task").findOne({
+            _id: task._id
+        })
+        .then((result)=>{
+
+            Object.keys(task).forEach(element => {
+                result[element] = task[element];
+            });
+
+            result.save()
+            .then((newResult)=>{
+                resolve(newResult);
+            })
+            .catch((err)=>{
+                reject(err);
+            });
+
+        })
+        .catch((err)=>{
+            reject(err);
+        });
+    });
+}
 
 module.exports = mongoose.models.Task || mongoose.model("Task", taskSchema);

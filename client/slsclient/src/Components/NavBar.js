05775e39d515050dab5f90178214570342ac52b5
Vinoshen Silva <VinoshenSilva@gmail.com>
2019-11-22 01:20:07 +0800

    Made the navbar responsive

--- client/slsclient/src/Components/NavBar.js
+++ client/slsclient/src/Components/NavBar.js
@@ -113,34 +113,41 @@ export class NavBar extends Component {
     }
   }
 
+
+
   renderContent(){
 
     return (
       <div>
+
           <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-
-              <Link to = "/" className="navbar-brand">Home</Link>
-
-              <button
-                  className="navbar-toggler"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#navbarColor01"
-                  aria-controls="navbarColor01"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-              >
-
+            
+            <Link to = "/" className="navbar-brand">Home</Link>
+            {/* <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="true" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
+            </button> */}
 
-            </button>
-          
-            <div className="collapse navbar-collapse" id="navbarColor01">
-              
-              <ul className="navbar-nav mr-auto"></ul>
-
+            <div className="navbar-collapse collapse show" id="navbarColor01">
+              {/* <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Features</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Pricing</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">About</a>
+                </li>
+              </ul> */}
+              {/* <form className="form-inline my-2 my-lg-0">
+                <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
+                <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+              </form> */}
               {this.props.loading ? this.renderLoading(): this.renderSearchBar()}
-
             </div>
           </nav>
         </div>
@@ -150,6 +157,8 @@ export class NavBar extends Component {
   render() {
     return (
       <div>
+
+
         {this.renderContent()}
       </div>
     );
